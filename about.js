2b6b677adc86c15e160cf54bdc8e632a0beb74bd
HtetMyatAungg <Htet.Aung.2025@live.rhul.ac.uk>
2026-04-26 19:06:29 +0100

    changes

---
+++ about.js
@@ -0,0 +1,13 @@
+const hire = document.getElementById("hire-me")
+hire.style.backgroundColor = "#534AB7"
+hire.style.borderRadius = "5px"
+hire.addEventListener("click", function() {
+    window.open("https://www.linkedin.com/in/htet-myat-aung-4a370932a/", "_blank", "noopener,noreferrer")
+})
+
+const cvButton = document.getElementsByClassName("CVButton")[0]
+if (cvButton) {
+    cvButton.addEventListener("click", function() {
+        window.open("https://drive.google.com/file/d/1vX0FUemX7igfGN29Lf-QfLNFq5MMTZrH/view?usp=drive_link", "_blank", "noopener,noreferrer")
+    })
+}
